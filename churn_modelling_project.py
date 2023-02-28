1ffa0ef22f7787aca99c1abc9623736c3278b909
jovin24 <49245390+jovin24@users.noreply.github.com>
2023-02-28 21:13:25 +0800

    Update churn_modelling_project.py

--- churn_modelling_project.py
+++ churn_modelling_project.py
@@ -14,3 +14,18 @@ df['Exited'] = df['Exited'].astype('category')
 df = df.drop('Balance', axis=1)
 
 print(df)
+
+
+rom sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix
+
+confusion_matrix = confusion_matrix(y_test, y_pred_LR)
+
+print('Logistic Regression')
+print('Confusion Matrix:\n' , confusion_matrix)
+
+print('\nAccuracy:', accuracy_score(y_test, y_pred_LR))
+print('Precision:', precision_score(y_test, y_pred_LR))
+print('Recall:', recall_score(y_test, y_pred_LR))
+print('F1-score:', f1_score(y_test, y_pred_LR))
+max_EstimatedSalary = df1.EstimatedSalary.max()
