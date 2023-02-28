eb8cb186737bbe7abdb9c2037598cf4c702f638d
jovin24 <49245390+jovin24@users.noreply.github.com>
2023-02-28 21:34:51 +0800

    Update churn_modelling_project.py

--- churn_modelling_project.py
+++ churn_modelling_project.py
@@ -38,4 +38,9 @@ Y = df1['Exited']
 X = df1.drop(['Exited', 'CustomerId' , 'EstimatedSalary', 'CreditScore'], axis=1)
 X_train, X_test, y_train, y_test = train_test_split(X,Y,train_size=0.8, random_state =0)
 
-st.write(y_test)
+st.write("The shape of X_train is:", X_train.shape)
+st.write("The shape of X_test is:", X_test.shape)
+
+st.write('')
+st.write("The shape of Y_train is:", y_train.shape)
+st.write("The shape of Y_test is:", y_test.shape)
