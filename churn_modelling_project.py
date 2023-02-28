ed0cba1a60cc68d738d7b7e782f752fddc8031b6
jovin24 <49245390+jovin24@users.noreply.github.com>
2023-02-28 21:32:59 +0800

    Update churn_modelling_project.py

--- churn_modelling_project.py
+++ churn_modelling_project.py
@@ -37,5 +37,5 @@ Y = df1['Exited']
 X = df1.drop(['Exited', 'CustomerId' , 'EstimatedSalary', 'CreditScore'], axis=1)
 X_train, X_test, y_train, y_test = train_test_split(X,Y,train_size=0.8, random_state =0)
 
-
-st.write(df1)
+x_train
+X_test
