42c8cb7e831147792a53deed3c67a27a49461878
jovin24 <49245390+jovin24@users.noreply.github.com>
2023-02-28 21:18:15 +0800

    Update churn_modelling_project.py

--- churn_modelling_project.py
+++ churn_modelling_project.py
@@ -5,6 +5,7 @@ import seaborn as sns
 
 
 df = pd.read_csv('Churn_Modelling.csv', index_col = 'CustomerId')
+"""# **Model Evaluation and Visualization**"""
 
 df['HasCrCard'] = df['HasCrCard'].astype('category')
 df['IsActiveMember'] = df['IsActiveMember'].astype('category')
@@ -15,17 +16,3 @@ df = df.drop('Balance', axis=1)
 
 print(df)
 
-
-rom sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from sklearn.metrics import confusion_matrix
-
-confusion_matrix = confusion_matrix(y_test, y_pred_LR)
-
-print('Logistic Regression')
-print('Confusion Matrix:\n' , confusion_matrix)
-
-print('\nAccuracy:', accuracy_score(y_test, y_pred_LR))
-print('Precision:', precision_score(y_test, y_pred_LR))
-print('Recall:', recall_score(y_test, y_pred_LR))
-print('F1-score:', f1_score(y_test, y_pred_LR))
-max_EstimatedSalary = df1.EstimatedSalary.max()
