7843f622f112c8215f37c78a5a42e65e9511c371
jovin24 <49245390+jovin24@users.noreply.github.com>
2023-02-28 21:26:52 +0800

    Update churn_modelling_project.py

--- churn_modelling_project.py
+++ churn_modelling_project.py
@@ -13,8 +13,6 @@ df['Exited'] = df['Exited'].astype('category')
 
 df = df.drop('Balance', axis=1)
 
-df
-
 import scipy.stats as stats
 
 Z_score_CreditScore = stats.zscore(df['CreditScore'])
@@ -22,7 +20,14 @@ Z_score_CreditScore = stats.zscore(df['CreditScore'])
 len(df[(Z_score_CreditScore<-3) | (Z_score_CreditScore>3)])
 
 df1= df[(Z_score_CreditScore>-3) & (Z_score_CreditScore<3)].reset_index()
+
+
+# Standardization for CreditScore
+avg_CreditScore = df1['CreditScore'].mean()
+std_CreditScore = df1['CreditScore'].std()
+
+df1['Z_Score_CreditScore'] = (df1['CreditScore'] - avg_CreditScore)/std_CreditScore
+zscore_rate=s.zscore(df1['CreditScore'])
+
+
 df1
-
-
-
