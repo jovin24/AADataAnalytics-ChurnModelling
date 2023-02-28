f085773e26774d1450e12ddf725a7ffc8ec901cb
jovin24 <49245390+jovin24@users.noreply.github.com>
2023-02-28 21:32:33 +0800

    Update churn_modelling_project.py

--- churn_modelling_project.py
+++ churn_modelling_project.py
@@ -36,4 +36,6 @@ from sklearn.model_selection import train_test_split
 Y = df1['Exited']
 X = df1.drop(['Exited', 'CustomerId' , 'EstimatedSalary', 'CreditScore'], axis=1)
 X_train, X_test, y_train, y_test = train_test_split(X,Y,train_size=0.8, random_state =0)
-st.write(df)
+
+
+st.write(df1)
