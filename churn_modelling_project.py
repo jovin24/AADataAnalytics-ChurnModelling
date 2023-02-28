dd3a17d89b93797e66d713295dbc97a441e8b296
jovin24 <49245390+jovin24@users.noreply.github.com>
2023-02-28 21:11:33 +0800

    Update churn_modelling_project.py

--- churn_modelling_project.py
+++ churn_modelling_project.py
@@ -13,29 +13,4 @@ df['Exited'] = df['Exited'].astype('category')
 
 df = df.drop('Balance', axis=1)
 
-df.info()
-
-#Calculate Z Score to treat Outliers
-import scipy.stats as stats
-
-Z_score_CreditScore = stats.zscore(df['CreditScore'])
-
-len(df[(Z_score_CreditScore<-3) | (Z_score_CreditScore>3)])
-
-df1= df[(Z_score_CreditScore>-3) & (Z_score_CreditScore<3)].reset_index()
-print(df1)
-
-# Standardization for CreditScore
-avg_CreditScore = df1['CreditScore'].mean()
-std_CreditScore = df1['CreditScore'].std()
-
-df1['Z_Score_CreditScore'] = (df1['CreditScore'] - avg_CreditScore)/std_CreditScore
-
-import scipy.stats as s
-zscore_rate=s.zscore(df1['CreditScore'])
-
-
-min_EstimatedSalary = df1.EstimatedSalary.min()
-max_EstimatedSalary = df1.EstimatedSalary.max()
-
-print(df1)
+print(df)
